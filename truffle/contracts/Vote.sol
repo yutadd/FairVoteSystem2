4bd43f2e2df5762ade3fa6c4154f0c61a8be6a3e
yutadd <spyk4m49@gmail.com>
2022-10-22 16:21:52 +0900

    Refactoring to reduce transaction fees

--- truffle/contracts/Vote.sol
+++ truffle/contracts/Vote.sol
@@ -10,7 +10,6 @@ pragma solidity >= 0.4 .22 < 0.9 .0;
 事前に作成されたリストをもとに、アドレスを生成し、addVoterしてください。
 どのアドレスが投票を行ったかはブロックチェーンエクスプローラーで確認できる。
 vote関数では投票先が空文字であることを検査し、revoteでは再投票で、得票数が+-0になるので投票のかさ増しはできない。
-
 */
 
 contract Vote {
@@ -31,82 +30,61 @@ contract Vote {
 	constructor() {
 		owner = msg.sender;
 	}
-	function addTarget(string memory _display) public returns(bool){
+	function addTarget(string memory _display) external returns(bool){
 		require(msg.sender == owner);
 		bytes memory b=bytes(_display);
 		for(uint a=0;a<b.length;a++){
 			if(b[a]==0x3c||b[a]==0x27||b[a]==0x3e||b[a]==0x22)return false;
 		}
 		targetList.push(_display);
-		targetDisplay[_display]=uint(0);
+		targetDisplay[_display]=uint(1);
 		return true;
 	}
-	function getAmountOfVotes(string memory _display) public view returns(uint){
-		return targetDisplay[_display];
+	function getAmountOfVotes(string memory _display) external view returns(uint){
+		return targetDisplay[_display]-1;
 	}
-	function getTargetArrayLength() public view returns(uint){
+	function getTargetArrayLength() external view returns(uint){
 		return targetList.length;
 	}
-	function getTargetDisplay(uint _index) public view returns(string memory){
+	function getTargetDisplay(uint _index) external view returns(string memory){
 		return targetList[_index];
 	}
-	function addVoter(address _voter) public{
+	function addVoter(address _voter) external{
 		require(msg.sender == owner);
 			voterList.push(_voter);
 			voter[_voter]="";
 	}
-	function getVoterArrayLength() public view returns(uint){
+	function getVoterArrayLength() external view returns(uint){
 		return voterList.length;
 	}
-	function getVoterAddress(uint i) public view returns (address){
+	function getVoterAddress(uint i) external view returns (address){
 		return voterList[i];
 	}
-	function getVotedTarget(address _voter) public view returns (string memory){
+	function getVotedTarget(address _voter) external view returns (string memory){
 		return voter[_voter];
 	}
-	function vote(string memory _target) public returns(bool) {
-		if (keccak256(abi.encodePacked(voter[msg.sender])) == keccak256(abi.encodePacked(""))) {//revoteであることがあとからわかるように、revoteとvoteは別関数
-			bool _exist;
-			for (uint i = 0; i < targetList.length; i++) {
-				if (keccak256(abi.encodePacked(targetList[i])) == keccak256(abi.encodePacked(_target))) {
-					_exist = true;
-					break;
-				}
-			}
-			if (_exist) {
-				targetDisplay[_target] += 1;
-				voter[msg.sender] = _target;
-				return true;
-			}
-			
-		}
-		return false;
+	function vote(string memory _target) external returns(bool) {
+		require(keccak256(abi.encodePacked(voter[msg.sender])) == keccak256(abi.encodePacked("")));//revoteであることがあとからわかるように、revoteとvoteは別関数
+			require(targetDisplay[_target]!=0);
+			targetDisplay[_target] += 1;
+			voter[msg.sender] = _target;
+			return true;
 	}
 	//this function is exist for fair voting.
-	function revote(string memory _target) public returns(bool){
-		if (keccak256(abi.encodePacked(voter[msg.sender])) != keccak256(abi.encodePacked(""))) {//revoteであることがあとからわかるように、revoteとvoteは別関数
-			bool _exist;
-			for (uint i = 0; i < targetList.length; i++) {
-				if (keccak256(abi.encodePacked(targetList[i])) == keccak256(abi.encodePacked(_target))) {
-					_exist = true;
-					break;
-				}
-			}
-			if (_exist) {
-				targetDisplay[voter[msg.sender]] -= 1;
-				targetDisplay[_target] += 1;
-				voter[msg.sender] = _target;
-				return true;
-			}
-		}
-		return false;
+	function revote(string memory _target) external returns(bool){
+		require(keccak256(abi.encodePacked(voter[msg.sender])) != keccak256(abi.encodePacked("")));//revoteであることがあとからわかるように、revoteとvoteは別関数
+			require(targetDisplay[_target]!=0);
+			targetDisplay[voter[msg.sender]] -= 1;
+			targetDisplay[_target] += 1;
+			voter[msg.sender] = _target;
+			return true;
 	}
 
-	function close() public {
+	function close() external {
 		require(msg.sender == owner);
 		closed = true;
 	}
-	function open() public {
+	function open() external {
 		require(msg.sender == owner);
 		closed = false;
 	}
